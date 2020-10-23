ae3333bab7bef796fa3327936d4908fd410c99c5
Jannik Bauknecht <Jannik_Bauknecht@web.de>
2020-10-23 15:49:10 +0200

    Changed Movement to Rigiedbody

--- Assets/ThirdPersonMovment.cs
+++ Assets/ThirdPersonMovment.cs
@@ -1,15 +1,18 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class ThirdPersonMovment : MonoBehaviour
 {
-    public CharacterController controller;
+    public Rigidbody body;
     public Transform cam;
     public Transform groundCheck;
     public float groundDistance = 0.4f;
     public LayerMask groundMask;
-    public float jumpHeight = 3f; 
+    public float jumpForce = 3f;
+    public float maxSpeed = 10f;
+    
 
     public float speed = 6f;
 
@@ -20,6 +23,8 @@ public class ThirdPersonMovment : MonoBehaviour
     Vector3 velocity;
     bool isGrounded;
     float turnSmoothVelocity;
+    Vector3 moveDir;
+    Vector3 direction;
 
     private void Start()
     {
@@ -30,20 +35,14 @@ public class ThirdPersonMovment : MonoBehaviour
     void Update()
     {
         isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
-        if(isGrounded && velocity.y < 0)
-        {
-            velocity.y = -2f;
-        }
-        velocity.y += gravity * Time.deltaTime;
-        controller.Move(velocity * Time.deltaTime);
         if (Input.GetButtonDown("Jump") && isGrounded)
         {
-            velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
+            body.AddForce(0, jumpForce, 0);
         }
 
         float horizontal = Input.GetAxisRaw("Horizontal");
         float vertical = Input.GetAxisRaw("Vertical");
-        Vector3 direction = new Vector3(horizontal, 0f, vertical).normalized;
+        direction = new Vector3(horizontal, 0f, vertical).normalized;
 
         if(direction.magnitude >= 0.1f)
         {
@@ -51,8 +50,21 @@ public class ThirdPersonMovment : MonoBehaviour
             float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothTime);
 
             transform.rotation = Quaternion.Euler(0f, targetAngle, 0f);
-            Vector3 moveDir = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
-            controller.Move(moveDir * speed * Time.deltaTime);
+            moveDir = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
+            //body.velocity = moveDir * speed * Time.deltaTime;
+            
+            //controller.Move(moveDir * speed * Time.deltaTime);
         }
+    }
+
+    void FixedUpdate()
+    {
+        if (direction.magnitude >= 0.1f && body.velocity.magnitude <= maxSpeed)
+        {
+
+            body.AddForce(moveDir * speed * Time.fixedDeltaTime);
+            Debug.Log(body.velocity);
+        }
+        //if (direction.magnitude >= 0.1f) body.MovePosition(body.position + moveDir * speed * Time.fixedDeltaTime);
     }
 }
