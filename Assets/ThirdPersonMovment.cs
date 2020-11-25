ac6939807e211b830d8d89e95c6279492b2a73b9
Jannik Bauknecht <Jannik_Bauknecht@web.de>
2020-11-25 15:34:13 +0100

    added first level, changed movement back to Controller, added Jump Obstacle, added moving Obstacle

--- Assets/ThirdPersonMovment.cs
+++ Assets/ThirdPersonMovment.cs
@@ -1,30 +1,32 @@
-﻿using System;
+﻿using Cinemachine.Utility;
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class ThirdPersonMovment : MonoBehaviour
 {
-    public Rigidbody body;
+    //public Rigidbody body;
     public Transform cam;
     public Transform groundCheck;
     public float groundDistance = 0.4f;
     public LayerMask groundMask;
-    public float jumpForce = 3f;
+    public float jumpHeight = 3f;
     public float maxSpeed = 10f;
+    public CharacterController controller;
     
 
     public float speed = 6f;
 
     public float turnSmoothTime = 0.1f;
-
-    public float gravity = -9.81f;
     
-    Vector3 velocity;
-    bool isGrounded;
+    public bool isGrounded;
     float turnSmoothVelocity;
-    Vector3 moveDir;
-    Vector3 direction;
+    public Vector3 moveDir;
+    public Vector3 direction;
+    public Vector3 bodyVelocity;
+    Vector3 velocity;
+    public float gravity = -9.81f;
 
     private void Start()
     {
@@ -35,9 +37,16 @@ public class ThirdPersonMovment : MonoBehaviour
     void Update()
     {
         isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+        if (isGrounded && velocity.y < 0)
+        {
+            velocity.y = -2f;
+        }
+        velocity.y += gravity * Time.deltaTime;
+        controller.Move(velocity * Time.deltaTime);
         if (Input.GetButtonDown("Jump") && isGrounded)
         {
-            body.AddForce(0, jumpForce, 0);
+            //body.AddForce(0, jumpForce, 0);
+            velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
         }
 
         float horizontal = Input.GetAxisRaw("Horizontal");
@@ -53,18 +62,24 @@ public class ThirdPersonMovment : MonoBehaviour
             moveDir = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
             //body.velocity = moveDir * speed * Time.deltaTime;
             
-            //controller.Move(moveDir * speed * Time.deltaTime);
+            controller.Move(moveDir * speed * Time.deltaTime);
         }
     }
 
     void FixedUpdate()
     {
-        if (direction.magnitude >= 0.1f && body.velocity.magnitude <= maxSpeed)
+        //Vector3 xzVelocity = new Vector3(body.velocity.x, 0f, body.velocity.z);
+        //if (direction.magnitude >= 0.1f && body.velocity.magnitude <= maxSpeed)
         {
-
-            body.AddForce(moveDir * speed * Time.fixedDeltaTime);
-            Debug.Log(body.velocity);
-        }
+            
+            //body.AddForce(moveDir * speed * Time.fixedDeltaTime, ForceMode.Impulse);
+            
+            //body.velocity = moveDir * speed * Time.fixedDeltaTime * 0.1f;
+        } /*else if(xzVelocity.magnitude >= 0.1f && direction.magnitude == 0)
+        {
+            body.AddForce(-body.velocity * speed * Time.fixedDeltaTime * 0.5f, ForceMode.Impulse);
+            Debug.Log(xzVelocity);
+        }*/
         //if (direction.magnitude >= 0.1f) body.MovePosition(body.position + moveDir * speed * Time.fixedDeltaTime);
     }
 }
---
+++ Assets/objectMover.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class objectMover : MonoBehaviour
+{
+
+    public CharacterController controller;
+    //public float direction = 1;
+    public Vector3 direction;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        controller.Move(direction);
+        direction.x = Mathf.Sin(Time.time) * 0.1f;
+    }
+}
