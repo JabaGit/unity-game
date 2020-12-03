b4efacebb403682e2c1cacee253c526d5a5a8ee3
toscit08 <tobias.schiemer@web.de>
2020-12-03 22:29:52 +0100

    Prototyp of New Player with Movement Animations

---
+++ Assets/AnimationStateController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimationStateController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    Animator animator;
+
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        bool forwardPressed = Input.GetKey("w");
+        bool leftPressed = Input.GetKey("a");
+        bool backwardPressed = Input.GetKey("s");
+        bool rightPressed = Input.GetKey("d");
+        if(forwardPressed || leftPressed || backwardPressed || rightPressed){
+            animator.SetBool("IsRunning", true);
+
+        }
+        else {
+             animator.SetBool("IsRunning", false);
+        }
+    }
+}
